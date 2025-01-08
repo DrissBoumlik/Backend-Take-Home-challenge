f362860b47e5a7b29a32b76e94df41bf855e88eb
Driss <drissboumlik.dev@gmail.com>
2025-01-08 22:10:12 +0100

    Add Unit test for api for listing all articles

--- tests/Unit/ArticleServiceTest.php
+++ tests/Unit/ArticleServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Article;
 use App\Services\ArticleService;
 use App\Services\NewsAggregatorService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -24,5 +25,23 @@ class ArticleServiceTest extends TestCase
         $service->fetchArticlesFromAPIs();
 
         $apiFetcherMock->shouldHaveReceived('importArticles');
+    }
+
+
+    public function test_get_all_articles(): void
+    {
+        Article::factory()->count(3)->create();
+
+        $service = app(ArticleService::class);
+
+        $articles = $service->getAllArticles();
+
+        $this->assertCount(3, $articles);
+        $this->assertArrayHasKey('title', $articles->first()->toArray());
+        $this->assertArrayHasKey('content', $articles->first()->toArray());
+        $this->assertArrayHasKey('author', $articles->first()->toArray());
+        $this->assertArrayHasKey('source', $articles->first()->toArray());
+        $this->assertArrayHasKey('category', $articles->first()->toArray());
+        $this->assertArrayHasKey('published_at', $articles->first()->toArray());
     }
 }
