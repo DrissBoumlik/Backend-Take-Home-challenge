6fdc31d11e62a1f2fde45766c2dc0fe9d1fdb486
Driss <drissboumlik.dev@gmail.com>
2025-01-10 21:21:03 +0100

    Add exception handling to user preferences index action

--- app/Http/Controllers/UserPreferenceController.php
+++ app/Http/Controllers/UserPreferenceController.php
@@ -6,16 +6,22 @@ use App\Http\Resources\UserPreferenceResource;
 use App\Models\UserPreference;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserPreferenceController extends Controller
 {
     public function index(Request $request)
     {
-         $user = Auth::user();
-         $preferences = UserPreference::where('user_id', $user->id)->get();
+        try {
+            $user = Auth::user();
 
-        // $preferences = UserPreference::inRandomOrder()->get(); // testing purposes
+            $preferences = UserPreference::where('user_id', $user->id)->get();
 
-        return response()->json(UserPreferenceResource::collection($preferences));
+            return response()->json(UserPreferenceResource::collection($preferences));
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to retrieve user preferences',
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
