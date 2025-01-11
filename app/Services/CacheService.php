6ee7fc0017bacb8f77d960c601f78f99541e8b7f
Driss <drissboumlik.dev@gmail.com>
2025-01-11 11:01:36 +0100

    Get default ttl inside the remember method

--- app/Services/CacheService.php
+++ app/Services/CacheService.php
@@ -6,12 +6,6 @@ use App\Config\CacheTTLConfig;
 use Illuminate\Support\Facades\Cache;
 class CacheService
 {
-    public const CACHE_EXPIRATION = CacheTTLConfig::DEFAULT_TTL_IN_SECONDS;
-
-    public function __construct()
-    {
-
-    }
 
     public static function remember(string $key, int|null $expiration, $callback, $forget = false)
     {
@@ -19,7 +13,7 @@ class CacheService
             Cache::forget($key);
         }
 
-        $expiration = $expiration ?: self::CACHE_EXPIRATION;
+        $expiration = CacheTTLConfig::getTtlInSeconds($expiration);
 
         return Cache::remember($key, $expiration, $callback);
     }
