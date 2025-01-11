007527556049afa3ea35365b987e22a02e1d1e08
Driss <drissboumlik.dev@gmail.com>
2025-01-11 21:45:18 +0100

    Add feature test to cover source api exceptions

--- tests/Feature/ImportArticlesTest.php
+++ tests/Feature/ImportArticlesTest.php
@@ -6,6 +6,7 @@ use Domain\NewsApis\Services\DataSourceManager;
 use Domain\NewsApis\Services\NewsAggregatorService;
 use Domain\NewsApis\Services\Sources\GuardianService;
 use Domain\NewsApis\Services\Sources\NewsApiService;
+use Domain\NewsApis\Services\Sources\NYTimesService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
 use Mockery;
@@ -66,5 +67,32 @@ class ImportArticlesTest extends TestCase
 
         $this->assertDatabaseCount('articles', 0);
     }
+
+    public function test_fetch_articles_handles_failed_response()
+    {
+        config()->set('news-sources.newsapi.config.url', 'https://mock-newsapi.org/v2/top-headlines');
+        config()->set('news-sources.guardian.config.url', 'https://mock-content.guardianapis.com/search');
+        config()->set('news-sources.ny_times.config.url', 'https://mock-api.nytimes.com/svc/topstories/v2/home.json');
+
+        $sources = [
+            NewsApiService::class,
+            GuardianService::class,
+            NYTimesService::class
+        ];
+
+        foreach ($sources as $source) {
+            $mockService = \Mockery::mock($source);
+            $mockService->allows('fetchArticles')
+                ->andThrow(new \Exception('Failed to fetch articles.'));
+
+            $this->app->instance($source, $mockService);
+
+            $sourceClass = new $source;
+            $sourceClass->fetchArticles();
+
+            $this->assertEmpty($sourceClass->getArticles());
+        }
+    }
+
 
 }
