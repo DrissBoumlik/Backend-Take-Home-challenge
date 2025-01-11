3c85681155c607fdc75dcd8103bc302d7efbe507
Driss <drissboumlik.dev@gmail.com>
2025-01-11 14:34:01 +0100

    Add exception handling and test for unauthenticated user

--- app/Http/Controllers/UserPreferenceController.php
+++ app/Http/Controllers/UserPreferenceController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Services\CacheService;
 use App\Services\User\UserPreferenceService;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserPreferenceController extends Controller
@@ -24,7 +26,12 @@ class UserPreferenceController extends Controller
                 return $this->userPreferenceService->getPreferences();
 
             }, $request->has('forget'));
-        } catch (\Exception $e) {
+        } catch (AuthenticationException $e) {
+            Log::warning('Unauthenticated user: ' . $e->getMessage());
+
+            return response()->json(['message' => 'Unauthenticated user', ], Response::HTTP_UNAUTHORIZED);
+        } catch (\Throwable $e) {
+            Log::error('Error in getting user preferences: ' . $e->getMessage(), [ 'exception' => $e ]);
             return response()->json([
                 'message' => 'Failed to retrieve user preferences',
             ], Response::HTTP_BAD_REQUEST);
--- app/Services/User/UserPreferenceService.php
+++ app/Services/User/UserPreferenceService.php
@@ -6,6 +6,7 @@ namespace App\Services\User;
 use App\Http\Resources\UserPreferenceResource;
 use App\Models\UserPreference;
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -15,19 +16,23 @@ class UserPreferenceService
 {
 
     /**
-     * @throws Exception
+     * @throws Throwable
      */
     public function getPreferences(): AnonymousResourceCollection
     {
         try {
             $user = Auth::user();
 
+            if (! $user) {
+                throw new AuthenticationException('Unauthenticated user');
+            }
+
             $preferences = UserPreference::where('user_id', $user->id)->get();
 
             return UserPreferenceResource::collection($preferences);
         } catch (Throwable $e) {
             Log::error('Error in UserPreferenceService: ' . $e->getMessage());
-            throw new Exception('Failed retrieving user preferences: ' . $e->getMessage(), 0, $e);
+            throw $e;
         }
 
     }
